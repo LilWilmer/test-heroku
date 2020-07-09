32ef811240c1f61aab53bfd16f4b733f94883ae6
EggyBoi <william.payne123@hotmail.com>
2020-07-09 16:09:19 +1000

    Using Express, Added Cool Faces

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 //const hostname = '127.0.0.1';
 const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
@@ -9,8 +9,21 @@ const server = http.createServer((req, res) => {
 /* Non heroku
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});*/
+});*
 
 server.listen(PORT, () => {
     console.log(`Server running on ${PORT}/`);
 });
+*/
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
